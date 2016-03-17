7d9f4f41024cd7dcd2f1fc99b2d9b1d5dc6394ac
Suraj Jorwekar <surajjorwekar@gmail.com>
2016-03-18 02:16:54 +0530

    New File added

--- slr_parser.py
+++ slr_parser.py
@@ -1,7 +1,26 @@
 #!/usr/bin/python
+class Stack:
+     def __init__(self):
+         self.items = []
+
+     def isEmpty(self):
+         return self.items == []
+
+     def push(self, item):
+         self.items.append(item)
+
+     def pop(self):
+         return self.items.pop()
+
+     def peek(self):
+         return self.items[len(self.items)-1]
+
+     def size(self):
+         return len(self.items)
+
 
 class Lex:
-	delim = [" ",",",";","{","}","(",")"]
+	delim = [" ",",",";","{","}","(",")","#"]
 	operator = ["+","-","/","*","=","&&","||","&","|","!","=="]
 
 	keywords=["auto","break","case","char","const","continue","default","do","double","else","enum","extern","float","for","goto","if","int","long","register","return","short","signed","sizeof","static","struct","switch","typedef","union","unsigned","void","volatile","while"]
@@ -11,28 +30,27 @@ class Lex:
 	invalid_operator = []
 	output = []
 	comment = 0
+	CURRENT=""
+	MAIN_TABLE = {}
 
-	actionTable = 
-	[
-		['0','s5','-','-','s4','-','-'],
-		['1','-','s6','-','-','-','acc'],
-		['2','-','r2','s7','-','r2','r2'],
-		['3','-','r4','r4','-','r4','r4'],
-		['4','s5','-','-','s4','-','-'],
-		['5','-','r6','r6','-','r6','r6'],
-		['6','s5','-','-','s4','-','-'],
-		['7','s5','-','-','s4','-','-'],
-		['8','-','s6','-','-','s11','-'],
-		['9','-','r1','s7','-','r1','r1'],
-		['10','-','r3','r3','-','r3','r3'],
-		['11','-','r5','r5','-','r5','r5']
-
-
-
+	actionTable = [
+		['s5','-','-','s4','-','-'],
+		['-','s6','-','-','-','acc'],
+		['-','r2','s7','-','r2','r2'],
+		['-','r4','r4','-','r4','r4'],
+		['s5','-','-','s4','-','-'],
+		['-','r6','r6','-','r6','r6'],
+		['s5','-','-','s4','-','-'],
+		['s5','-','-','s4','-','-'],
+		['-','s6','-','-','s11','-'],
+		['-','r1','s7','-','r1','r1'],
+		['-','r3','r3','-','r3','r3'],
+		['-','r5','r5','-','r5','r5']
 	]
 
-	gotoTable = 
-	[
+	
+
+	gotoTable = [
 		[1,2,3],
 		[-1,-1,-1],
 		[-1,-1,-1],
@@ -48,6 +66,43 @@ class Lex:
 
 	]
 	
+
+	def initHash(self):
+		k=o=j=0;
+		l['id','+','*','(',')','$','E','T','F']
+
+		for i in self.actionTable:
+			data = i[j]
+			if o == len(l):
+				o=0
+			self.MAIN_TABLE[k,l[o]]
+			o+=1
+			k+=1
+			j+=1
+
+
+
+	def F(self,state,curr):
+		self.actionTable[]
+		return
+
+	def G(self,state,curr):
+		return
+
+
+	def parser(self,token,STACK):
+		CURRENT = token
+		Status = self.F(STACK.peek(),CURRENT)
+
+		if "s" in Status:
+			Status = Status[1]
+			STACK.push(Status)
+
+		elif "r" in Status:
+			print "None"
+		
+
+		#print STACK.peek(),token
 
 	def isEnd(self,lexeme):
 		if lexeme in self.delim or lexeme in self.operator :
@@ -104,46 +159,61 @@ class Lex:
 
 
 
-	def splitToken(self,data):
+	def splitToken(self,data,S):
 		lexeme = ""
 		j=0
 		length = len(data)
+		
 		while j != length:
 			ch = data[j]
-			#print j,ch
+			#print ch
 
 
 
 			if ch in self.delim or ch in self.operator: 
 				#print "Lex : ",lexeme
+
 				if lexeme != "":
 					found = self.find(lexeme)
 					if found != "1":
 						self.lexeme_list.append(lexeme+" \t "+found)
-
+						final_lexa = lexeme
+						self.parser(final_lexa,S)
+						final_lexa = ""
 				if ch != "" and ch != " ":
 					if ch in self.delim:
 						self.lexeme_list.append(ch+" \t Delimiter")
+						final_lexb = ch
 					else:
 						if ch+data[j+1] in self.operator:
 							self.lexeme_list.append(ch+data[j+1]+" \t Operator")
+							final_lexb = ch+data[j+1]
 							j+=1
 						elif ch in self.operator:
 							self.lexeme_list.append(ch+" \t Operator")
+							final_lexb = ch
 						else:
 						    self.invalid_operator.append(ch)
+					self.parser(final_lexb,S)
+					final_lexb = ""
+
 				lexeme = ""
+				
 
 			else:
 				lexeme+= ch
 
+
+			
 			j+=1
 			
 			
 L = Lex()
+
 fo = open("example.c","r")
 
-
+S = Stack()
+S.push("0")
 while 1:
 
 	data = fo.readline().strip()
@@ -156,10 +226,10 @@ while 1:
 	elif "//" in data:
 		continue
 	else:
-		L.splitToken(data)
-		if data == "}":
+		L.splitToken(data,S)
+		if data == "":
 			break
-
+'''
 print "\n\n    -------------------------\n        LEXEME    TOKEN\n    -------------------------"
 for ch in L.lexeme_list:		
 	print "\t",ch
@@ -173,10 +243,6 @@ print "\n\n    -------------------------\n      INVALID IDENTIFIER\n    --------
 
 for ch in L.invalid_identifier:
 	print "\t",ch
-'''
-print "\n\n    -------------------------\n      INVALID OPERATOR\n    -------------------------"
 
-for ch in L.invalid_operator:
-	print "\t",ch
-	'''
 print "\n\n"
+'''
